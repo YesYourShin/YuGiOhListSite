6a554211c35a545b327b0c8e5babb21d176329c2
YesYourShin <ehrl827@naver.com>
2021-09-14 23:19:59 +0900

    insert card data

---
+++ YGODataInsert/index.js
@@ -0,0 +1,62 @@
+const fs = require('fs');
+const axios = require('axios').default
+const delay = (ms) => new Promise((resolve) => setTimeout(() => resolve(), ms))
+
+fs.readFile('./src/input/out.json', 'utf8', (error, jsonFile) => {
+    if (error) return console.log(error);
+    // console.log(jsonFile);
+
+    const jsonData = JSON.parse(jsonFile); 
+    // console.log(jsonFile);
+
+    // console.log(jsonData.length); // 배열 길이
+
+    
+    load(jsonData)
+
+    // const todos = jsonData.todos;
+    // todos.forEach(todo => {
+    //     console.log(todo);
+    // });
+});
+
+async function load(jsonData) {
+console.log('load')
+    let count = 0;
+    for(let i = 0; i <= jsonData.length; i++){
+        
+        if (i%100==0 && i !=0) {
+            let data = jsonData.slice(i-100, i);
+            count += 1;
+            cardDataOutput(data, count);
+            cardDataInsert(data);
+
+        }
+
+        if (i == jsonData.length) {
+            let data = jsonData.slice(((count*100)+1), i);
+            cardDataOutput(data, count);
+            cardDataInsert(data);
+        }
+        await delay(50)
+    }
+}
+
+function cardDataInsert(data) {
+
+    const cardData = JSON.stringify(data);
+    axios.post('http://127.0.0.1:8000/insert', cardData,{
+        headers:{"Content-Type" : "application/json"}
+    })
+    // .then(function (response) {
+    //     console.log(response);
+    // })
+    .catch(function (error) {
+        console.log(error);
+    });
+}
+
+function cardDataOutput(data, count) {
+    const cardData = JSON.stringify(data);
+    fs.writeFileSync(`./src/output/output${parseInt(count)}.json`,cardData);
+}
