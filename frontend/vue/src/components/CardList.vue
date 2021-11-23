6db58e3e2c887b4494476c415beb9fb7fec0975d
YesYourShin <ehrl827@naver.com>
2021-11-23 20:33:30 +0900

    cleaning code

--- frontend/vue/src/components/CardList.vue
+++ frontend/vue/src/components/CardList.vue
@@ -4,64 +4,62 @@
             :items="cards"
             :items-per-page="itemsPerPage"
             :page.sync="currentPage"
-            
             :search="search"
             :sort-by="sortBy.toLowerCase()"
             :sort-desc="sortDesc"
             hide-default-footer
         >
-
-<template v-slot:header>
-    <v-toolbar
-        dark
-        color="blue darken-3"
-        class="mb-1"
-    >
-        <v-text-field
-        v-model="search"
-        clearable
-        flat
-        solo-inverted
-        hide-details
-        prepend-inner-icon="mdi-magnify"
-        label="Search"
-        ></v-text-field>
-        <template v-if="$vuetify.breakpoint.mdAndUp">
-        <v-spacer></v-spacer>
-        <v-select
-            v-model="sortBy"
-            flat
-            solo-inverted
-            hide-details
-            :items="keys"
-            prepend-inner-icon="mdi-magnify"
-            label="Sort by"
-        ></v-select>
-        <v-spacer></v-spacer>
-        <v-btn-toggle
-            v-model="sortDesc"
-            mandatory
-        >
-            <v-btn
-            large
-            depressed
-            color="blue"
-            :value="false"
-            >
-            <v-icon>mdi-arrow-up</v-icon>
-            </v-btn>
-            <v-btn
-            large
-            depressed
-            color="blue"
-            :value="true"
-            >
-            <v-icon>mdi-arrow-down</v-icon>
-            </v-btn>
-        </v-btn-toggle>
-        </template>
-    </v-toolbar>
-    </template>
+            <template v-slot:header>
+                <v-toolbar
+                    dark
+                    color="blue darken-3"
+                    class="mb-1"
+                >
+                    <v-text-field
+                    v-model="search"
+                    clearable
+                    flat
+                    solo-inverted
+                    hide-details
+                    prepend-inner-icon="mdi-magnify"
+                    label="Search"
+                    ></v-text-field>
+                    <template v-if="$vuetify.breakpoint.mdAndUp">
+                        <v-spacer></v-spacer>
+                        <v-select
+                            v-model="sortBy"
+                            flat
+                            solo-inverted
+                            hide-details
+                            :items="keys"
+                            prepend-inner-icon="mdi-magnify"
+                            label="Sort by"
+                        ></v-select>
+                        <v-spacer></v-spacer>
+                        <v-btn-toggle
+                            v-model="sortDesc"
+                            mandatory
+                        >
+                            <v-btn
+                            large
+                            depressed
+                            color="blue"
+                            :value="false"
+                            >
+                                <v-icon>mdi-arrow-up</v-icon>
+                            </v-btn>
+                            <v-btn
+                            large
+                            depressed
+                            color="blue"
+                            :value="true"
+                            >
+                                <v-icon>mdi-arrow-down</v-icon>
+                            </v-btn>
+                        </v-btn-toggle>
+                    </template>
+                </v-toolbar>
+            </template>
 
             <template v-slot:default="props">
                 <v-row>
@@ -74,29 +72,31 @@
                         lg="3"
                     >
                         <v-card>
-                            <v-card-title><router-link to="/">{{ card.title }}</router-link></v-card-title>
-<v-divider></v-divider>
+                            <v-card-title>
+                                <router-link to="/">{{ card.title }}</router-link>
+                            </v-card-title>
+                            <v-divider></v-divider>
                             <v-list dense>
-                <v-list-item
-                  v-for="(key, index) in filteredKeys"
-                  :key="index"
-                >
-                  <v-list-item-content :class="{ 'blue--text': sortBy === key }">
-                    {{ key }}:
-                  </v-list-item-content>
-                  <v-list-item-content
-                    class="align-end"
-                    :class="{ 'blue--text': sortBy === key }"
-                  >
-                    {{ card[key.toLowerCase()] }}
-                  </v-list-item-content>
-                </v-list-item>
-              </v-list>
+                                <v-list-item
+                                v-for="(key, index) in filteredKeys"
+                                :key="index"
+                                >
+                                    <v-list-item-content :class="{ 'blue--text': sortBy === key }">
+                                        {{ key }}:
+                                    </v-list-item-content>
+                                    <v-list-item-content
+                                        class="align-end"
+                                        :class="{ 'blue--text': sortBy === key }"
+                                    >
+                                        {{ card[key.toLowerCase()] }}
+                                    </v-list-item-content>
+                                </v-list-item>
+                            </v-list>
                         </v-card>
-                        
                     </v-col>
                 </v-row>
             </template>
+
             <template v-slot:footer>
                 <v-row
                     class="mt-2"
@@ -120,7 +120,6 @@
         :length="lastPage"
         :total-visible="10"
         @input="getPage"/> -->
-
 
 </template>
 
@@ -163,8 +162,8 @@ export default {
     },
     computed: {
         filteredKeys () {
-        return this.keys.filter(key => key !== 'Name')
-      },
+            return this.keys.filter(key => key !== 'Name')
+        },
     },
     methods: {
         getPage(value) {
