ac800f45cd6abd9a97485cf9f70c584d165edf89
YesYourShin <ehrl827@naver.com>
2022-06-08 20:35:34 +0900

    sql dataInsert

--- YGODataInsert/index.js
+++ YGODataInsert/index.js
@@ -1,24 +1,56 @@
 const fs = require('fs');
+const mysql = require('mysql');
+const connection = mysql.createConnection({
+    host: 'localhost',
+    user: 'root',
+    password: 'rootuser',
+    database: 'yugiohtest',
+});
 const { mainModule } = require('process');
 const axios = require('axios').default;
 const delay = ms => new Promise(resolve => setTimeout(() => resolve(), ms));
 
 const main = async () => {
-    for (let i = 1; i <= 111; i++) {
-        fs.readFile(`../ygo-crawler/output-ja/output${i}.json`, 'utf8', (error, jsonFile) => {
+    connection.connect();
+    for (let i = 1; i <= 1; i++) {
+        fs.readFile(`../ygo-crawler/output-ja/output${i}.json`, 'utf8', (error, cards) => {
             if (error) return console.log(error);
+            const jsonFile = JSON.parse(cards);
+            for (const card of jsonFile) {
+                // 컬럼에 예약어와 동일한 이름의 컬럼이 있기 때문에 백틱으로 감싸주어야 한다.
+                // 백틱 안에서 백틱을 쓰는 방법을 찾지 못했다.
+                // card 객체에 있는 key 값을 따로 모아서 백틱 + key + 백틱 으로 해서 감싸주었다.
+                const columns = Object.keys(card)
+                    .map(key => '`' + key + '`')
+                    .join();
 
-            axios
-                .post('http://127.0.0.1:8000/insert', jsonFile)
-                .then(function (response) {
-                    // console.log(i);
-                    // console.log(response);
-                })
-                .catch(function (error) {
-                    console.log(error);
+                const values = Object.values(card)
+                    .map(key => '"' + key + '"')
+                    .join();
+
+                connection.query(`INSERT INTO cards(${columns}) VALUES(${values});`, (error, results, fields) => {
+                    if (error) return console.log(error);
+                    console.log(results);
                 });
+
+                // connection.query(
+                //     `INSERT INTO cards(
+                //         title, title2, limited, icon, attribute, `level`, `rank`, link, atk, def,
+                //         monsterType, cardType, pScale, pEffect, cardText) VALUES(
+                //     "${card.title}","${card.title2}","${card.limited}","${card.icon}","${card.attribute}","${card.level}",
+                //     "${card.rank}","${card.link}","${card.atk}","${card.def}","${card.monsterType}","${card.cardType}",
+                //     "${card.pScale}","${card.pEffect}","${card.cardText}"
+                //     );`,
+                //     (error, results, fields) => {
+                //         console.log(error);
+                //     }
+                // );
+            }
         });
         await delay(1000);
     }
+    // connection.query('select * from cards', (error, result) => {
+    //     console.log(result);
+    // });
 };
 main();
