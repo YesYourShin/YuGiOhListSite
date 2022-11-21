39d5677d85e055c96b4c95a12f2df00ce64c552f
YesYourShin <ehrl827@naver.com>
2022-11-22 00:39:38 +0900

    ADD select lang for change card lang or not found

--- project/frontend/src/components/CardItem.vue
+++ project/frontend/src/components/CardItem.vue
@@ -1,23 +1,28 @@
 <template>
-  <div v-if="card">
-    <p>CardItem</p>
-    <div>{{ code }} 페이지 입니다.</div>
-    <div class="flex">
-      <div>
-        <img :src="require(`../assets/black_magic_girl.jpg`)" />
+  <div>
+    <div v-if="card != 'Not Found' && card">
+      <p>CardItem</p>
+      <div>{{ code }} 페이지 입니다.</div>
+      <div class="flex">
+        <div>
+          <img :src="require(`../assets/black_magic_girl.jpg`)" />
+        </div>
+
+        <div>
+          <p>Name: {{ card.name }}</p>
+          <p v-if="this.getLang == 'ja'">Hiragana: {{ card.name2 }}</p>
+          <p>Card Text: {{ card.card_text }}</p>
+        </div>
       </div>
 
-      <div>
-        <p>Name: {{ card.name }}</p>
-        <p v-if="this.getLang == 'ja'">Hiragana: {{ card.name2 }}</p>
-        <p>Card Text: {{ card.card_text }}</p>
+      <div v-for="item in card.cardNumber" v-bind:key="item.code">
+        <p>Card Number: {{ item.card_number }}</p>
+        <p>Pack Name: {{ item.pack_name }}</p>
+        <p>Rare: {{ item.rare }}</p>
       </div>
     </div>
-
-    <div v-for="item in card.cardNumber" v-bind:key="item.code">
-      <p>Card Number: {{ item.card_number }}</p>
-      <p>Pack Name: {{ item.pack_name }}</p>
-      <p>Rare: {{ item.rare }}</p>
+    <div v-if="card == 'Not Found'">
+      <p>Not Found</p>
     </div>
   </div>
 </template>
@@ -47,6 +52,7 @@ export default {
   },
   methods: {
     getCard() {
+      this.card = null;
       axios
         .get(`/api/card/${this.getLang}/show/${this.code}`)
         .then(response => {
